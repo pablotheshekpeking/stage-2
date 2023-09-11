a219a42f582dc26b6ae54cafb3ec84a704c17b46
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-11 14:44:36 +0300

    nav

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,35 +1,52 @@
 'use client'
-import { Box, Stack, Img, Heading, Input, InputGroup, InputRightElement  } from "@chakra-ui/react";
-import { SearchIcon } from '@chakra-ui/icons';
+import { Box, Stack, Img, Heading, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
+import { SearchIcon, HamburgerIcon } from '@chakra-ui/icons';
 
 const Navbar = () => {
-    return ( 
+    return (
 
         <Stack direction={'row'}>
-        <Box width={'20px'}></Box>
-        <Box className='navbar'>
-          <Stack direction={'row'}>
-            <Stack direction={'row'} className='logo'>
-              <Img src='' />
-              <Heading color={'white'}>
-                MovieBox
-              </Heading>
-            </Stack>
-
-            {/** search bar */}
-            <InputGroup>
-              <Input type='tel' placeholder='What do you want to watch?' />
-              <InputRightElement pointerEvents='none'>
-                <SearchIcon color='gray.300' />
-              </InputRightElement>
-            </InputGroup>
+            <Box width={'20px'}></Box>
+            <Box className='navbar'>
+                <Stack direction={'row'}>
+                    <Box>
+                        <Stack direction={'row'} className='logo'>
+                            <Img src='' />
+                            <Heading color={'white'}>
+                                MovieBox
+                            </Heading>
+                        </Stack>
+                    </Box>
 
 
-          </Stack>
-        </Box>
-        <Box width={'20px'}></Box>
-      </Stack>
-     );
+                    {/** search bar */}
+                    <Box>
+                        <InputGroup>
+                            <Input type='tel' placeholder='What do you want to watch?' width={'100&'} />
+                            <InputRightElement pointerEvents='none'>
+                                <SearchIcon color='gray.300' />
+                            </InputRightElement>
+                        </InputGroup>
+                    </Box>
+
+
+                    <Stack>
+                    <Box>
+                        <Text>
+                            Sign in
+                        </Text>
+                    </Box>
+                    <Box>
+                        <HamburgerIcon borderRadius={'30px'} bg={'red.500'} color={'white'} />
+                    </Box>
+                    </Stack>
+
+
+                </Stack>
+            </Box>
+            <Box width={'20px'}></Box>
+        </Stack>
+    );
 }
- 
+
 export default Navbar;
