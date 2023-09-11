825311e52481a60de9239170e2a7d60e5fa2e932
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-11 14:30:05 +0300

    search bar

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -1,6 +1,7 @@
 'use client'
 
-import { Stack, Flex, Box, Button, Heading, Text, VStack, useBreakpointValue } from '@chakra-ui/react';
+import { Stack, Flex, Box, Img, Button, Heading, Input, InputGroup, InputRightElement, Text, VStack, useBreakpointValue } from '@chakra-ui/react';
+import { SearchIcon } from '@chakra-ui/icons';
 
 export default function Hero() {
   return (
@@ -15,7 +16,31 @@ export default function Hero() {
       <Stack direction={'row'}>
         <Box width={'20px'}></Box>
         <Box>
-          <Stack maxW={'2xl'} spacing={6} mt={'40%'}>
+          <Stack direction={'row'}>
+            <Box width={'20px'}></Box>
+            <Box className='navbar'>
+              <Stack direction={'row'}>
+                <Stack direction={'row'} className='logo'>
+                  <Img src='' />
+                  <Heading>
+                    MovieBox
+                  </Heading>
+                </Stack>
+
+                {/** search bar */}
+                <InputGroup>
+                  <Input type='tel' placeholder='What do you want to watch?' />
+                  <InputRightElement pointerEvents='none'>
+                    <SearchIcon color='gray.300' />
+                  </InputRightElement>
+                </InputGroup>
+
+
+              </Stack>
+            </Box>
+            <Box width={'20px'}></Box>
+          </Stack>
+          <Stack maxW={'2xl'} spacing={6} mt={'40%'} className='hero-info'>
             <Heading
               color={'white'}
               fontWeight={700}
