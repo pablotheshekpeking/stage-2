c0d45ea43fef8f46d72e9ce1359d0c221a1713a8
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-11 21:31:47 +0300

    footer

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -7,32 +7,32 @@ import React from "react";
 const Footer = () => {
     return (
         <>
-            <Stack direction={'row'} ml={'45%'}>
+            <Stack direction={'row'} ml={'45%'} mb={'20px'}>
                 <FaFacebook />
                 <FaInstagram />
                 <FaTwitter />
                 <FaYoutube />
             </Stack>
 
-            <Stack direction={'row'} textAlign={'center'}>
-                <Box>
-                    <Text>
+            <Stack direction={'row'} textAlign={'center'} mb={'20px'}>
+                <Box w={'33%'}>
+                    <Text textAlign={'center'}>
                         Conditions of Use
                     </Text>
                 </Box>
-                <Box>
-                    <Text>
+                <Box w={'33%'}>
+                    <Text textAlign={'center'}>
                         Privacy & Policy
                     </Text>
                 </Box>
-                <Box>
-                    <Text>
+                <Box w={'33%'}>
+                    <Text textAlign={'center'}>
                         Press Room
                     </Text>
                 </Box>
             </Stack>
 
-            <Stack textAlign={'center'}>
+            <Stack textAlign={'center'} mb={'20px'}>
                 <Text textAlign={'center'}>
                     2021 MovieBox by Adriana Eka Prayduha 
                 </Text>
