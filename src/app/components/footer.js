2073f026b113fb7f04ca428db1d77e7bd033db5b
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-11 21:29:04 +0300

    footer

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -5,14 +5,40 @@ import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 import React from "react";
 
 const Footer = () => {
-    return ( 
-        <Stack direction={'row'}  ml={'45%'}>
-            <FaFacebook />
-            <FaInstagram ml={'10%'} />
-            <FaTwitter ml={'10%'} />
-            <FaYoutube ml={'10%'} />
-        </Stack>
-     );
+    return (
+        <>
+            <Stack direction={'row'} ml={'45%'}>
+                <FaFacebook />
+                <FaInstagram />
+                <FaTwitter />
+                <FaYoutube />
+            </Stack>
+
+            <Stack direction={'row'} textAlign={'center'}>
+                <Box>
+                    <Text>
+                        Conditions of Use
+                    </Text>
+                </Box>
+                <Box>
+                    <Text>
+                        Privacy & Policy
+                    </Text>
+                </Box>
+                <Box>
+                    <Text>
+                        Press Room
+                    </Text>
+                </Box>
+            </Stack>
+
+            <Stack textAlign={'center'}>
+                <Text textAlign={'center'}>
+                    2021 MovieBox by Adriana Eka Prayduha 
+                </Text>
+            </Stack>
+        </>
+    );
 }
- 
+
 export default Footer;
