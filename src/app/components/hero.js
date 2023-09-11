2b2073308109f6c593aa3f1da89ef0a37b6d8bce
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-11 13:45:05 +0300

    hero image

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -8,7 +8,7 @@ export default function Hero() {
       w={'full'}
       h={'100vh'}
       backgroundImage={
-        'url(https://images.unsplash.com/photo-1600267175161-cfaa711b4a81?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80)'
+        'url(https://static-prod.adweek.com/wp-content/uploads/2019/05/john-wick-poster-qa-hed-page-2019.jpg)'
       }
       backgroundSize={'cover'}
       backgroundPosition={'center center'}>
