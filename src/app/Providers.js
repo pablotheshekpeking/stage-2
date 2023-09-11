ddf51021ce3e2475ff70b3b1c5973cd2ac6d0a98
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-11 12:13:07 +0300

    first commit 2

--- src/app/Providers.js
+++ src/app/Providers.js
@@ -1,12 +1,15 @@
 // app/providers.js
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
+import  Hero  from './components/hero';
 
 export function Providers({ children }) {
   return (
     <CacheProvider>
       <ChakraProvider>
-        {children}
+
+        <Hero />
+
       </ChakraProvider>
     </CacheProvider>
   );
