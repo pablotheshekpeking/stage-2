8263ca4622607c309ebf1d62a532779a1d5e3380
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-11 22:37:50 +0300

    new hero

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -1,54 +1,77 @@
-'use client'
-
 import { Stack, Flex, Box, Img, Button, Heading, Input, InputGroup, InputRightElement, Text, VStack, useBreakpointValue } from '@chakra-ui/react';
 import { ChevronRightIcon } from '@chakra-ui/icons';
 import { FaPlayCircle } from 'react-icons/fa';
 import Navbar from './navbar';
 
-
 export default function Hero() {
   return (
-    <><Flex
-      w={'full'}
-      h={'90vh'}
-      backgroundImage={'url(https://static-prod.adweek.com/wp-content/uploads/2019/05/john-wick-poster-qa-hed-page-2019.jpg)'}
-      backgroundSize={'cover'}
-      backgroundPosition={'center center'}>
-      <Stack direction={'row'}>
-        <Box width={'20px'}></Box>
-        <Box>
-          <Navbar />
-          <Stack maxW={'2xl'} spacing={6} mt={'20%'} className='hero-info' ml={['', '', '100px', '100px']}>
-            <Heading
-              color={'white'}
-              fontWeight={700}
-              lineHeight={1.2}
-              fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}>
-              John Wick 3: Parabellum
-            </Heading>
-            <Text color={'white'}>
-              John Wick is on the run after killing a memeber of the international
-              assasins' guild and with a $14 million price tag on his head, he is the target
-              of hit men and women everywhere.
-            </Text>
-            <Stack direction={'row'}>
-              <Button
-                bg={'red.400'}
-                rounded={'full'}
+    <>
+      <Flex
+        w={'full'}
+        h={'90vh'}
+        backgroundImage={'url(https://static-prod.adweek.com/wp-content/uploads/2019/05/john-wick-poster-qa-hed-page-2019.jpg)'}
+        backgroundSize={'cover'}
+        backgroundPosition={'center center'}
+        position="relative" // Add this line
+      >
+        {/* Add an overlay with a semi-transparent background color */}
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          width="100%"
+          height="100%"
+          backgroundColor="rgba(0, 0, 0, 0.5)" // Adjust the opacity (0.5 in this example)
+        ></Box>
+
+        <Stack direction={'row'}>
+          <Box width={'20px'}></Box>
+          <Box>
+            <Navbar />
+            <Stack
+              maxW={'2xl'}
+              spacing={6}
+              mt={'20%'}
+              className='hero-info'
+              ml={['', '', '100px', '100px']}
+            >
+              <Heading
                 color={'white'}
-                _hover={{ bg: 'red.500' }}>
-                <FaPlayCircle />  Watch Trailer
-              </Button>
+                fontWeight={700}
+                lineHeight={1.2}
+                fontSize={useBreakpointValue({ base: '3xl', md: '4xl' })}
+              >
+                John Wick 3: Parabellum
+              </Heading>
+              <Text color={'white'}>
+                John Wick is on the run after killing a member of the international
+                assassins' guild and with a $14 million price tag on his head, he is the target
+                of hit men and women everywhere.
+              </Text>
+              <Stack direction={'row'}>
+                <Button
+                  bg={'red.400'}
+                  rounded={'full'}
+                  color={'white'}
+                  _hover={{ bg: 'red.500' }}
+                >
+                  <FaPlayCircle /> Watch Trailer
+                </Button>
+              </Stack>
             </Stack>
-          </Stack>
-        </Box>
-        <Box width={'20px'}></Box>
-      </Stack>
-    </Flex>
+          </Box>
+          <Box width={'20px'}></Box>
+        </Stack>
+      </Flex>
 
-
-    {/** featured movies */}
-    <Stack direction={'row'} mr={['', '', '100px', '100px']} ml={['', '', '100px', '100px']} mt={['50px', '50px', '100px', '100px']} mb={['50px', '50px', '100px', '100px']}>
+      {/** featured movies */}
+      <Stack
+        direction={'row'}
+        mr={['', '', '100px', '100px']}
+        ml={['', '', '100px', '100px']}
+        mt={['50px', '50px', '100px', '100px']}
+        mb={['50px', '50px', '100px', '100px']}
+      >
         <Box w={'50%'}>
           <Heading fontSize={['12px', '30px', '30px', '30px']}>
             Featured Movie
@@ -60,6 +83,6 @@ export default function Hero() {
           </Text>
         </Box>
       </Stack>
-      </>
-  )
+    </>
+  );
 }
