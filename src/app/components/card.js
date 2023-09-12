09794a700f082a998cc3ce51f34626cb71089d77
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-12 15:01:58 +0300

    commit

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -2,26 +2,60 @@
 
 import { Img, Text, Heading, Stack, Box } from "@chakra-ui/react";
 import { CheckCircleIcon } from '@chakra-ui/icons';
-import React from "react";
+import React, { useEffect, useState } from "react";
+import tmdbApi from '../api'; // Import the tmdbApi instance
+
 const Card = () => {
-    return (
-        <Stack direction={'column'}>
-            <Box mb={'20px'} w={'full'}
-                h={['200px', '200px', '400px', '400px']}
-                backgroundImage={
-                    'url(batman.jpeg)'
-                }
-                backgroundSize={'cover'}
-                backgroundPosition={'center center'}>
-                    <CheckCircleIcon float={'right'} color={'white'} mt={'20px'} boxSize={6} mr={'20px'} />
-            </Box>
-            <Box>
-                <Text color={'#dddddd'} mb={'20px'}>USA, 2005</Text>
-                <Heading as={'h6'}>Batman Begins</Heading>
-                <Text color={'#dddddd'}>Action, Adventure</Text>
-            </Box>
+  const [topRatedMovies, setTopRatedMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchTopRatedMovies = async () => {
+      try {
+        // Fetch the top-rated movies and limit the results to 10
+        const response = await tmdbApi.get('/movie/top_rated', {
+          params: {
+            page: 1, // Page 1
+            language: 'en-US', // Language
+          },
+        });
+
+        // Get the first 10 movies from the results
+        const top10Movies = response.data.results.slice(0, 10);
+        
+        setTopRatedMovies(top10Movies);
+      } catch (error) {
+        console.error('Error fetching top-rated movies:', error);
+      }
+    };
+
+    fetchTopRatedMovies();
+  }, []);
+
+  return (
+    <>
+      {topRatedMovies.map((movie) => (
+        <Stack direction={'column'} key={movie.id}>
+          <Box
+            mb={'20px'}
+            w={'full'}
+            h={['200px', '200px', '400px', '400px']}
+            backgroundImage={`url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}
+            backgroundSize={'cover'}
+            backgroundPosition={'center center'}
+          >
+            <CheckCircleIcon float={'right'} color={'white'} mt={'20px'} boxSize={6} mr={'20px'} />
+          </Box>
+          <Box>
+            <Text color={'#dddddd'} mb={'20px'}>
+              {movie.release_date ? movie.release_date.slice(0, 4) : 'N/A'}
+            </Text>
+            <Heading as={'h6'}>{movie.title}</Heading>
+            <Text color={'#dddddd'}>{movie.genres.map((genre) => genre.name).join(', ')}</Text>
+          </Box>
         </Stack>
-    );
+      ))}
+    </>
+  );
 }
 
 export default Card;
