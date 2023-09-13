8de46263661e2296f96e14209524098c96b6ad1f
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-13 14:52:09 +0300

    try

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -1,32 +1,39 @@
 'use client'
 
 import { Text, Heading, Stack, Box } from "@chakra-ui/react";
-import { CheckCircleIcon } from '@chakra-ui/icons';
+import Link from 'next/link'; // Import Link from 'next/link'
 import { AiFillHeart } from 'react-icons/ai';
 import React from "react";
 
 const Card = ({ movie }) => {
+  // Construct the correct href for the details page
+  const href = `/details?id=${movie.id}`;
+
   return (
-    <a href="../details"><Stack direction={'column'} data-testid="movie-card">
-      <Box
-        mb={'20px'}
-        w={'full'}
-        h={['200px', '200px', '400px', '400px']}
-        backgroundImage={`url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}
-        backgroundSize={'cover'}
-        backgroundPosition={'center center'}
-        data-testid="movie-poster"
-      >
-        <AiFillHeart style={{float: "right", marginTop: "20px", marginBottom: "20px",}} />
-      </Box>
-      <Box>
-        <Text color={'#dddddd'} mb={'10px'} data-testid="movie-release-date">
-          {movie.release_date} 
-        </Text>
-        <Text as={'h6'} mb={'10px'} data-testid="movie-title">{movie.title}</Text>
-        <Text color={'#dddddd'}>{movie.genres?.map((genre) => genre.name).join(', ')}</Text>
-      </Box>
-    </Stack></a>
+    <Link href={href}>
+      <a> {/* Wrap the contents in an anchor tag */}
+        <Stack direction={'column'} data-testid="movie-card">
+          <Box
+            mb={'20px'}
+            w={'full'}
+            h={['200px', '200px', '400px', '400px']}
+            backgroundImage={`url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}
+            backgroundSize={'cover'}
+            backgroundPosition={'center center'}
+            data-testid="movie-poster"
+          >
+            <AiFillHeart style={{float: "right", marginTop: "20px", marginRight: "20px", color: "white"}} />
+          </Box>
+          <Box>
+            <Text color={'#dddddd'} mb={'10px'} data-testid="movie-release-date">
+              {movie.release_date} 
+            </Text>
+            <Text as={'h6'} mb={'10px'} data-testid="movie-title">{movie.title}</Text>
+            <Text color={'#dddddd'}>{movie.genres?.map((genre) => genre.name).join(', ')}</Text>
+          </Box>
+        </Stack>
+      </a>
+    </Link>
   );
 }
 
--- src/app/details.js
+++ src/app/details.js
@@ -1,10 +1,61 @@
+// pages/details.js
 'use client'
-import Sidebar from "./components/sidebar";
+
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Box, Text } from '@chakra-ui/react';
 
 const Details = () => {
-    return ( 
-        <Sidebar />
-     );
-}
- 
+  const router = useRouter();
+  const { id } = router.query;
+  const [movieDetails, setMovieDetails] = useState(null);
+
+  useEffect(() => {
+    // Fetch movie details using the `id` from the query parameter
+    if (id) {
+      // Make an API request to fetch movie details by `id` (replace with your API logic)
+      // Example:
+      // fetch(`/api/movies/${id}`)
+      //   .then((response) => response.json())
+      //   .then((data) => setMovieDetails(data))
+      //   .catch((error) => console.error('Error fetching movie details:', error));
+
+      // For demonstration purposes, simulate fetching movie details
+      const simulatedMovieDetails = {
+        title: 'Sample Movie',
+        release_date: '2023-01-01',
+        genres: [{ name: 'Action' }, { name: 'Adventure' }],
+        backdrop_path: '/example.jpg',
+      };
+      setMovieDetails(simulatedMovieDetails);
+    }
+  }, [id]);
+
+  return (
+    <Box p={4}>
+      {movieDetails ? (
+        <>
+          <Box
+            mb={4}
+            w="full"
+            h={['200px', '200px', '400px', '400px']}
+            backgroundImage={`url(https://image.tmdb.org/t/p/original${movieDetails.backdrop_path})`}
+            backgroundSize="cover"
+            backgroundPosition="center center"
+          />
+          <Text fontSize="2xl" fontWeight="bold">
+            {movieDetails.title}
+          </Text>
+          <Text color="#dddddd" mb={4}>
+            Release Date: {movieDetails.release_date}
+          </Text>
+          <Text>{movieDetails.genres?.map((genre) => genre.name).join(', ')}</Text>
+        </>
+      ) : (
+        <Text>Loading...</Text>
+      )}
+    </Box>
+  );
+};
+
 export default Details;
