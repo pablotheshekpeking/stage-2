082c5f4a426d66e92ec485b7d772c5ae272bd129
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-14 17:30:19 +0300

    go

--- src/app/movies/[id]/page.js
+++ src/app/movies/[id]/page.js
@@ -437,7 +437,7 @@ export default async function MoviePage({ params }) {
                     borderRadius={'10px'}
                     w={'full'}
                     h={'50px'}
-                    mt={'230px'}
+                    mt={'200px'}
                     bg={'rgba(18, 18, 18, 0.50)'}
                     backdropFilter={'blur(2px)'}
 
