49d21b274f88043ea042cb81f42d3bdf40eb4ccc
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-14 13:22:15 +0300

    tom done

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -1,7 +1,7 @@
 'use client'
 
-import { Text, Heading, Stack, Box, Img } from "@chakra-ui/react";
-import  Link  from 'next/link';
+import { Text, Heading, Stack, Box, Img, Show } from "@chakra-ui/react";
+import Link from 'next/link';
 import { AiFillHeart } from 'react-icons/ai';
 import React, { useState } from "react";
 
@@ -14,30 +14,31 @@ const Card = ({ movie }) => {
 
   return (
     <Link href={`/movies/${movie.id}`}>
-        <Stack direction={'column'} data-testid="movie-card">
-          <Box
-            mb={'20px'}
-            w={'full'}
-            h={['200px', '200px', '400px', '400px']}
-            backgroundImage={`url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}
-            backgroundSize={'cover'}
-            backgroundPosition={'center center'} 
-            data-testid="movie-poster"
-          >
-            <AiFillHeart
-              style={{
-                float: "right",
-                marginTop: "20px",
-                marginRight: "20px",
-                color: isClicked ? "red" : "white" // Change color to red when clicked
-              }}
-              onClick={toggleHeartColor} // Toggle the color when clicked
-            />
-          </Box>
-          <Box>
-            <Text color={'#dddddd'} mb={'10px'} data-testid="movie-release-date">
-              {movie.release_date}
-            </Text>
+      <Stack direction={'column'} data-testid="movie-card">
+        <Box
+          mb={'20px'}
+          w={'full'}
+          h={['200px', '200px', '400px', '400px']}
+          backgroundImage={`url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`}
+          backgroundSize={'cover'}
+          backgroundPosition={'center center'}
+          data-testid="movie-poster"
+        >
+          <AiFillHeart
+            style={{
+              float: "right",
+              marginTop: "20px",
+              marginRight: "20px",
+              color: isClicked ? "red" : "white" // Change color to red when clicked
+            }}
+            onClick={toggleHeartColor} // Toggle the color when clicked
+          />
+        </Box>
+        <Box>
+          <Text color={'#dddddd'} mb={'10px'} data-testid="movie-release-date">
+            {movie.release_date}
+          </Text>
+          <Show above='lg'>
             <Stack direction={'row'} m={'auto'}>
               <Box w={'33%'}>
                 <Stack direction={'row'}>
@@ -53,10 +54,11 @@ const Card = ({ movie }) => {
                 </Stack>
               </Box>
             </Stack>
-            <Text as={'h6'} mb={'10px'} data-testid="movie-title">{movie.title}</Text>
-            <Text color={'#dddddd'}>{movie.genres?.map((genre) => genre.name).join(', ')}</Text>
-          </Box>
-        </Stack>
+          </Show>
+          <Text as={'h6'} mb={'10px'} data-testid="movie-title">{movie.title}</Text>
+          <Text color={'#dddddd'}>{movie.genres?.map((genre) => genre.name).join(', ')}</Text>
+        </Box>
+      </Stack>
     </Link>
   );
 }
